2daea8ab37c8386eae703b3ab4be8016758d8085
alvaro-asouza <alvaroalves928@gmail.com>
2025-02-20 15:22:48 -0300

    adicionando componentes, navbar e footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,26 @@ import Home from './components/pages/Home'
 import Contact from './components/pages/Contact'
 import Company from './components/pages/Company'
 import NewProject from './components/pages/NewProject'
+import Projects from './components/pages/Projects'
 
 import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <Router>
-      <Link to="/">Home</Link>
-      <Link to="/contact">Contact</Link>
-      <Link to="/company">Company</Link>
-      <Link to="/newproject">New Project</Link>
+      <Navbar />
       <Container customClass="min-height">
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/projects" element={<Projects />} />
         <Route exact path="/company" element={<Company />} />
         <Route exact path="/newproject" element={<NewProject />} />
       </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer />
     </Router>
   )
 }
