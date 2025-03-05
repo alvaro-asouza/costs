c65012c9fcb4ae502d845317a2d42c147a96203a
alvaro-asouza <alvaroalves928@gmail.com>
2025-03-04 22:57:10 -0300

    adicionando loader na aba de projetos

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react"
 
 import Message from "../layout/Message"
 import Container from "../layout/Container"
+import Loading from "../layout/Loading"
 import LinkButton from "../layout/LinkButton"
 import ProjectCard from "../project/ProjectCard"
 
@@ -12,6 +13,7 @@ import styles from './Projects.module.css'
 
 function Projects() {
     const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
 
     const location = useLocation()
     let message = ''
@@ -21,18 +23,21 @@ function Projects() {
 
     useEffect(() => {
 
-        fetch('http://localhost:5000/projects', {
-            method: 'GET',
-            headers: {
-                'Content-Type' : 'application/json',
-            }
-        }).then(resp => resp.json())
-        .then(data => {
-            console.log(data);
-            
-            setProjects(data)
-        })
-        .catch(error => console.log(error))
+        setTimeout(() => {
+            fetch('http://localhost:5000/projects', {
+                method: 'GET',
+                headers: {
+                    'Content-Type' : 'application/json',
+                }
+            }).then(resp => resp.json())
+            .then(data => {
+                console.log(data);
+                
+                setProjects(data)
+                setRemoveLoading(true)
+            })
+            .catch(error => console.log(error))
+        }, 300)
             
     }, [])
 
@@ -54,6 +59,10 @@ function Projects() {
                         key={project.id}
                         />
                     ))
+                }
+                {!removeLoading && <Loading />}
+                {!removeLoading && projects.length === 0 &&
+                    <p>Não há projetos cadastrados!</p>
                 }
             </Container>
         </div>
